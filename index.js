fb6d71bbffe49e47bf5da4d8327a4bda87b093cb
Szelagi <44713495+Szelagi@users.noreply.github.com>
2023-05-06 22:45:17 +0200

    position normalize

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ import mineflayer from 'mineflayer';
 import {getYawFrom, length} from "./Vector/vector.js";
 import {Process} from "./System/process.js";
 
-
-
-
 import cluster from 'node:cluster';
 import process from 'node:process';
 
-const numCPUs = 8;
+const numCPUs = 1;
 const wait = (ms) => new Promise(r => setTimeout(r, ms));
 
 if (cluster.isPrimary) {
@@ -18,7 +15,7 @@ if (cluster.isPrimary) {
 
     (async ()=>{
         for (let i = 0; i < numCPUs; i++) {
-            await wait(6500);
+            //await wait(6500);
             cluster.fork();
         }
     })();
@@ -34,14 +31,19 @@ if (cluster.isPrimary) {
 
     const bot = mineflayer.createBot({
         host: 'localhost',
-        port: 25565,
-        username: `ProcesorCore${Math.floor(Math.random()*10000)}`,
+        port: 55979,
+        username: `ProcesorCore`,
         version: '1.19'
     });
 
-    bot.once('login', () => setTimeout(start, 2000));
+    bot.once('login', () => setTimeout(start, 500));
 
     async function start() {
+        await bot.chat('/tp -31.5 -37 -31.5');
+        await wait(1000);
+        await jump(bot, -31.5, -37, -28.5);
+
+    }
         // while (true) {
         // await wait(1500);
         // await go2D(12.5, -17.5);
@@ -51,26 +53,26 @@ if (cluster.isPrimary) {
         // await go2D(10.5, -11.5);
         // }
 
-        bot.chat(`/give ${bot.username} diamond_sword`);
-
-        bot.on('respawn', () => {
-            bot.chat(`/give ${bot.username} diamond_sword`);
-        })
-
-
-        const players = Object.keys(bot.players).filter(e => (!e.includes('ProcesorCore') && (e != 'Szelagi') ));
-        console.log(players)
-
-        const rPlayer = players[Math.floor(Math.random()*players.length)];
-
-        let nick = rPlayer;
-        while (true) {
-            bot.chat(`Idę do ${nick}`);
-            await follow2D(bot, nick);
-            bot.chat(`EZ`);
-            await wait(5000);
-        }
-    }
+    //     bot.chat(`/give ${bot.username} diamond_sword`);
+    //
+    //     bot.on('respawn', () => {
+    //         bot.chat(`/give ${bot.username} diamond_sword`);
+    //     })
+    //
+    //
+    //     const players = Object.keys(bot.players).filter(e => (!e.includes('ProcesorCore') && (e != 'Szelagi') ));
+    //     console.log(players)
+    //
+    //     const rPlayer = players[Math.floor(Math.random()*players.length)];
+    //
+    //     let nick = rPlayer;
+    //     while (true) {
+    //         bot.chat(`Idę do ${nick}`);
+    //         await follow2D(bot, nick);
+    //         bot.chat(`EZ`);
+    //         await wait(5000);
+    //     }
+    // }
 
 }
 
@@ -162,3 +164,116 @@ async function follow2D(bot, nick) {
         }, 750);
     });
 }
+
+
+async function rotateHead(bot, x, z) {
+    await bot.look(getYawFrom(bot.entity.position.x,bot.entity.position.z, x, z), 0, true);
+}
+
+
+async function read(bot, probe, count) {
+    const getX = () => bot.entity.position.x;
+    const getZ = () => bot.entity.position.z;
+    let lastX = getX();
+    let lastZ = getZ();
+    for(let i = 0; i < count; i++) {
+        await wait(probe);
+        console.log('x:', lastX-getX(), 'z:', lastZ-getZ());
+        lastX = getX();
+        lastZ = getZ();
+    }
+}
+
+
+async function normalizePosition(bot) {
+    // forward = Z-
+    // backward = Z+
+    // left = X-
+    // right = X+
+    const SNEAK_SPEED = 1.31;
+    const ACCEPTED_DEVIATION = 0.06;
+    const getDeviation = (position) => {
+        if (position === 0) return 0;
+        let comma = position - Math.floor(position);
+        if (comma < 0) comma += 1;
+        // comma range <0, 1)
+        return comma - 0.5;
+    }
+    const getTime = (deviation) => {
+        return (1 / SNEAK_SPEED) * Math.abs(deviation);
+    }
+    // Normalize X
+    const xDeviation = getDeviation(bot.entity.position.x);
+    console.log('def', xDeviation)
+    if (Math.abs(xDeviation) > ACCEPTED_DEVIATION) {
+        const time = getTime(xDeviation);
+        await bot.look(0, 0, true);
+        await bot.setControlState('sneak', true);
+        if (xDeviation > 0) {
+            await bot.setControlState('left', true);
+            await wait(time*1000);
+            await bot.setControlState('left', false);
+        } else {
+            await bot.setControlState('right', true);
+            await wait(time*1000);
+            await bot.setControlState('right', false);
+        }
+        await bot.setControlState('sneak', false);
+    }
+    const zDeviation = getDeviation(bot.entity.position.z);
+    console.log('defz', zDeviation)
+    if (Math.abs(zDeviation) > ACCEPTED_DEVIATION) {
+        const time = getTime(zDeviation);
+        await bot.look(0, 0, true);
+        await bot.setControlState('sneak', true);
+        if (zDeviation > 0) {
+            await bot.setControlState('forward', true);
+            await wait(time*1000);
+            await bot.setControlState('forward', false);
+        } else {
+            await bot.setControlState('back', true);
+            await wait(time*1000);
+            await bot.setControlState('back', false);
+        }
+        await bot.setControlState('sneak', false);
+    }
+}
+
+async function jump(bot, x, y, z) {
+    await normalizePosition(bot);
+    const getX = () => bot.entity.position.x;
+    const getZ = () => bot.entity.position.z;
+    const jumpLength = length(bot.entity.position.x, bot.entity.position.z, x, z);
+    await rotateHead(bot, x, z);
+    if (jumpLength < 2) {
+
+    } else {
+        bot.setControlState('sprint', true);
+        bot.setControlState('forward', true);
+        bot.setControlState('jump', true);
+        await wait(50);
+        bot.setControlState('jump', false);
+        await wait(550);
+        bot.setControlState('sprint', false);
+        bot.setControlState('forward', false);
+    }
+    // const time = (1 / 7.127) * jumpLength;
+    //
+    // const extendValue = 0.2;
+    // const getXCondition = () => getX() > x-extendValue && getX() < x+extendValue;
+    // const TICK_IN_MS = 50;
+    // const getZCondition = () => getZ() > z-extendValue && getZ() < z+extendValue;
+    // while ( !(getXCondition() && getZCondition()) ) {
+    //     console.log(getZCondition())
+    //     await wait(50);
+    //     console.log(bot.entity.position.z)
+    // }
+
+    await wait(1500);
+    console.log(bot.entity.position);
+}
+
+function getSprintJumpTime(length) {
+
+}
+
